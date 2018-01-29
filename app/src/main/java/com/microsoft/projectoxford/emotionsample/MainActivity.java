b2cce61f163356e38513c0e668f1255979f607d6
Sayed Hussaini <seh31628@uga.edu>
2018-01-28 21:00:46 -0500

    Update MainActivity.java
    Cleaning Up Code.

--- app/src/main/java/com/microsoft/projectoxford/emotionsample/MainActivity.java
+++ app/src/main/java/com/microsoft/projectoxford/emotionsample/MainActivity.java
@@ -55,21 +55,16 @@ public class MainActivity extends ActionBarActivity {
         }
 
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     public void activityRecognize(View v) {
         Intent intent = new Intent(this, RecognizeActivity.class);
         startActivity(intent);
     }
-
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
